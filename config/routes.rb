d559101bfe6ee699ebd18f459b7364cefd8fa6cf
Sofia Daouadji <sofia.daouadji@protonmail.com>
2023-05-23 14:30:34 -0400

    add a task

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  get '/index', to: 'tasks#index', as: :tasks
-  get '/show/:id', to: 'tasks#show', as: :task
+  get 'tasks', to: 'tasks#index', as: :tasks
+  get 'tasks/new', to: 'tasks#new', as: :new
+  get 'tasks/:id', to: 'tasks#show', as: :task
+  post 'tasks', to: 'tasks#create'
 end
