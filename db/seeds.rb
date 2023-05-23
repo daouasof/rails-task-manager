496b38579bcb4b08e180dd78b452dca7b1eaf859
Sofia Daouadji <sofia.daouadji@protonmail.com>
2023-05-23 12:09:48 -0400

    index

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+5.times do
+  Task.create(
+  title:Faker::Book.title,
+  details:Faker::Book.author,
+  completed: [true, false].sample
+  )
+  puts "creating an element"
+end
